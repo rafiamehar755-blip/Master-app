b2b61d4917f324272137a123d2396d654e1fc2cb
rafiamehar755-blip <rafiamehar755@gmail.com>
2026-07-21 17:11:58 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = 'ahmad-suite-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './quotation.html',
+  './invoice.html',
+  './receipt.html',
+  './manifest.json',
+  './Ahmad_Aluminium_Logo.png',
+  './Signature-Ahmad.png',
+  './Stamp-Ahmad.jpg'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  // Ignore Google Sheet URLs to avoid CORS problems
+  if (event.request.url.includes('google.com') || event.request.url.includes('googleusercontent.com')) {
+    return;
+  }
+  
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          if (response.status === 200) {
+            const responseClone = response.clone();
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, responseClone);
+            });
+          }
+          return response;
+        })
+        .catch(() => {
+          return caches.match(event.request);
+        })
+    );
+  }
+});
